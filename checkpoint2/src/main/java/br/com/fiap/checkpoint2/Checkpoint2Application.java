79bc0084d6adbf08777f9e38ba3365a47657c24d
Paulo <paulopocas20@gmail.com>
2026-04-24 10:35:44 -0300

    atualizando codigo

--- checkpoint2/src/main/java/br/com/fiap/checkpoint2/Checkpoint2Application.java
+++ checkpoint2/src/main/java/br/com/fiap/checkpoint2/Checkpoint2Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Checkpoint1Application {
+public class Checkpoint2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Checkpoint1Application.class, args);
+		SpringApplication.run(Checkpoint2Application.class, args);
 	}
 
 }
